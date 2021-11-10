79aaf86179e94e5884c9ab8723454ade121022c4
Arafat-Hossain-Anik <arafat15@cse.pstu.ac.bd>
2021-11-10 14:44:25 +0600

    api update

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors())
 app.use(express.json());
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vsocy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+//connection string
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.weuxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 async function run() {
@@ -20,12 +19,6 @@ async function run() {
         const serviceCollection = database.collection('places');
         const myOrderCollection = database.collection('usersOrders');
         console.log('database connected');
-        // // inserting data 
-        // app.get('/places', async (req, res) => {
-        //     const result = await serviceCollection.insertMany(places);
-        //     console.log(`${result.insertedCount} documents were inserted`);
-        //     res.send('Travel server is running');
-        // })
         // getting from database 
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find({})
